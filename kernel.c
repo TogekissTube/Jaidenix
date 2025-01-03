facbf254c2f8b1f03ead25ad89ed2d29e0fbe1bb
TogekissTube <138976298+TogekissTube@users.noreply.github.com>
2025-01-03 12:24:16 +0100

    Eliminate enable_paging(); reference
    I was just experimenting with Paging

---
+++ kernel.c
@@ -0,0 +1,13 @@
+#include "kernel/drivers/screen.h"
+#include "kernel/arch/i386/gdt.h"
+#include "kernel/arch/i386/idt.h"
+#include "kernel/arch/i386/interrupts.h"
+
+void main() {
+    screen_clear(); // Limpia la pantalla
+    init_gdt(); // Habilita la GDT
+    init_idt(); // Habilita la IDT
+    init_interrupts(); // Inicia el manejador de interrupciones
+    asm volatile("sti"); // Habilita las interrupciones
+    screen_write("Hello world!\n");
+}
